7cdba931a0128c04de4687fba921c23cd5dd197c
brek076 <38405777+brek076@users.noreply.github.com>
2022-10-30 03:09:00 +0300

    All in one

---
+++ app/src/main/java/com/example/jmashome/LogInActivity.java
@@ -0,0 +1,30 @@
+package com.example.jmashome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        Button signInButton = findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent( LogInActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/jmashome/ui/chat/ChatViewModel.java
@@ -0,0 +1,7 @@
+package com.example.jmashome.ui.chat;
+
+import androidx.lifecycle.ViewModel;
+
+public class ChatViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
